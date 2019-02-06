be9345f76453b7fea1940b48c119f6a23eed4e34
Luis Henrique Perin <luis.perin@hotmail.com>
2019-02-05 22:08:46 -0200

    resolvi o conflito

--- index.php
+++ index.php
@@ -1,13 +1,10 @@
 <?php
 
-<<<<<<< HEAD
-echo "Ola Mundo Velho";
 
-echo "Ola Perin";
-=======
 echo "Ola Mundo Novo";
 
-echo "Ola Luis";
->>>>>>> 3ef8c854d45b49418873313c96f6c16571a329fd
+echo "Ola Perin Luis";
+
+
 
 ?>
